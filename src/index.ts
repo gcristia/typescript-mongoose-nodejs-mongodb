c02ed297e059d547e3804b3c6ea0484064ee38bd
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2022-07-16 14:07:06 -0400

    Add Connection with MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World 2")
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    const db = await mongoose.connect('mongodb://localhost/typegoosedb')
+    console.log('MongoDB Connected ->',db.connection?.db.databaseName)
+}
+
+connectDB()
